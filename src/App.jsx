a42f9479fd7d448299196b6919d6462b3b3ef73e
Eduardo Rodriguez <tiago.edu.rodriguez@gmail.com>
2024-05-28 02:48:29 -0300

    Modicando Css e implementando nueva estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,49 @@
-import { useState,useRef } from 'react'
-import Nav from './components/Nav';
-
+import { useState, useRef } from "react";
+import Nav from "./components/Nav";
+import Slider from "./components/Slider";
+import SideBarItem from "./components/SideBarItem";
 function App() {
-  const [foto, setFoto] = useState(null);
-  const fileInputRef = useRef(null);
+    const [foto, setFoto] = useState(null);
+    const fileInputRef = useRef(null);
 
-  const cargarFoto = (event) =>{
-    const file = event.target.files[0];
-    if(file){
-      const reader = new FileReader();
-      reader.onload = (e) =>{
-        setFoto(e.target.result)
-      };
-      reader.readAsDataURL(file);
-    }
-  }
+    const cargarFoto = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setFoto(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
-  const handleClick = () => {
-    if(fileInputRef.current){
-      fileInputRef.current.click();
-    }
-  }
+    const handleClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
 
-  const eliminarFoto = () =>{
-    setFoto(null);
-    if(fileInputRef.current){
-      fileInputRef.current.value = "";
-    }
-  }
+    const eliminarFoto = () => {
+        setFoto(null);
+        if (fileInputRef.current) {
+            fileInputRef.current.value = "";
+        }
+    };
 
-  return (
-    <>
-    <Nav />
-      <div className="contenedor">
-        
-        {foto ? (
-          <div className='contenedor_imagen'>
-            <img className='fotoEditar' src={foto} alt="Foto cargada" />
-          </div>
-        ) : (
-          <p>La foto no esta cargada</p>
-        )}
-        <div className={`${foto ? 'botones_foto' : 'botones_sinFoto'}`}>
-          {foto ? (<button className='boton' onClick={eliminarFoto}>Eliminar Foto</button>) : ""}
-          <button className='boton' onClick={handleClick}>Subir Foto</button>
-        </div>
-        <input className='boton' type='file' accept='image/*' onChange={cargarFoto} ref={fileInputRef}></input>
-      </div>
-     
-    </>
-  )
+    return (
+        <>
+            <div className="contenedor">
+                <div className="main-image"></div>
+                <div className="sidebar">
+                    <SideBarItem />
+                    <SideBarItem />
+                    <SideBarItem />
+                    <SideBarItem />
+                </div>
+                <Slider />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/SideBarItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SideBarItem() {
+    return <button className="sidebar-item">Sidebar Item</button>;
+}
+
+export default SideBarItem;
